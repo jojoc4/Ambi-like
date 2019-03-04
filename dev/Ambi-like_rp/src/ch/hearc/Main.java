6b597868ae6130ae8802bb2f92d98a7ef86d3a5f
Jonatan Baumgartner <Jonatan.Baumgartner@he-arc.ch>
2019-03-04 14:35:35 +0100

    rmi server raspberrypi

--- dev/Ambi-like_rp/src/ch/hearc/Main.java
+++ dev/Ambi-like_rp/src/ch/hearc/Main.java
@@ -5,13 +5,10 @@
  */
 package ch.hearc;
 
-import java.net.InetAddress;
-import java.net.MalformedURLException;
-import java.net.UnknownHostException;
-import java.rmi.Naming;
-import java.rmi.RMISecurityManager;
-import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
 /**
  *
@@ -24,39 +21,17 @@ public class Main {
      */
     public static void main(String[] args) {
         try {
+            LedServerImp obj = new LedServerImp();
+            LedServer stub = (LedServer) UnicastRemoteObject.exportObject(obj, 0);
 
-            LocateRegistry.createRegistry(1099);
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("LedServer", stub);
 
-            System.out.println("Mise en place du Security Manager ...");
-
-            if (System.getSecurityManager() == null) {
-
-                System.setSecurityManager(new RMISecurityManager());
-
-            }
-
-            LedServerImp informationImpl = new LedServerImp();
-
-            String url = "rmi://" + InetAddress.getLocalHost().getHostAddress() + "/ledserver";
-
-            System.out.println("Enregistrement de l'objet avec l'url : " + url);
-
-            Naming.rebind(url, informationImpl);
-
-            System.out.println("Serveur lancé");
-
-        } catch (RemoteException e) {
-
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
             e.printStackTrace();
-
-        } catch (MalformedURLException e) {
-
-            e.printStackTrace();
-
-        } catch (UnknownHostException e) {
-
-            e.printStackTrace();
-
         }
     }
 
