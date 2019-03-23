37f7f2b09061454d76691d493d4af75f7f3a1c17
Julien Chappuis <julien.chappuis@he-arc.ch>
2019-03-23 14:33:09 +0100

    importation des bibliotheque et compilation statique pour le rpc

--- dev/Ambi-like_rp/src/ch/hearc/Main.java
+++ dev/Ambi-like_rp/src/ch/hearc/Main.java
@@ -10,6 +10,8 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+import com.github.mbelling.ws281x.*;
+
 /**
  *
  * @author jonatan.baumgart
@@ -20,6 +22,12 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Ws281xLedStrip led = new Ws281xLedStrip(10, 18, 800000, 10, 255, 0, false, LedStripType.WS2811_STRIP_RGB, false);
+
+        led.setStrip(100, 0, 100);
+
+        led.render();
+
         try {
             LedServerImp obj = new LedServerImp();
             LedServer stub = (LedServer) UnicastRemoteObject.exportObject(obj, 0);
