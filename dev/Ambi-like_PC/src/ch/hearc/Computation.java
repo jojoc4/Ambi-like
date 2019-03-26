ac8493fab0321afbcf2c0351ec29a9de14970732
Téo Schaffner <teo.schaffner@he-arc.ch>
2019-03-26 13:12:50 +0100

    recupérer couleur pixels

--- dev/Ambi-like_PC/src/ch/hearc/Computation.java
+++ dev/Ambi-like_PC/src/ch/hearc/Computation.java
@@ -5,6 +5,7 @@ import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -37,12 +38,44 @@ public class Computation implements Runnable {
     public void run() {
         BufferedImage img = printScreen();
         
-        int rgb = img.getRGB(0, 0);
-        int red =   (rgb >> 16) & 0xFF;
-        int green = (rgb >>  8) & 0xFF;
-        int blue =  (rgb      ) & 0xFF;
         
-        System.out.println("rouge: " + red + " vert: " + green + " bleu: " + blue);
+//        int red =   (rgb >> 16) & 0xFF;
+//        int green = (rgb >>  8) & 0xFF;
+//        int blue =  (rgb      ) & 0xFF;
+        //System.out.println("rouge: " + red + " vert: " + green + " bleu: " + blue);
+        
+        for(int i=0; i<nbLedLeft-1; ++i)
+        {
+                int rgb = img.getRGB(0, i*img.getHeight()/nbLedLeft);
+                colors[0][i] = rgb;
+        }
+        for(int i=0; i<nbLedTop-1; ++i)
+        {
+            int rgb = img.getRGB(i*img.getWidth()/nbLedTop, 0);
+            colors[1][i] = rgb;
+        }
+        for(int i=0; i<nbLedRight-1; ++i)
+        {
+            int rgb = img.getRGB(0, i*img.getHeight()/nbLedRight);
+            colors[2][i] = rgb;
+        }
+        for(int i=0; i<nbLedBottom-1; ++i)
+        {
+            int rgb = img.getRGB(i*img.getWidth()/nbLedBottom, 0);
+            colors[3][i] = rgb;
+        }
+        
+        for(int[] a : colors)
+        {
+            for(int b : a)
+            {
+                int red =   (b >> 16) & 0xFF;
+                int green = (b >>  8) & 0xFF;
+                int blue =  (b      ) & 0xFF;
+                System.out.println("rouge: " + red + " vert: " + green + " bleu: " + blue);
+            }
+            System.out.println("coté");
+        }
     }
 
     /**
