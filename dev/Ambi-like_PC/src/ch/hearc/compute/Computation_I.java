cd0f1e1e020534a0b92af0cfaeacfbb6aaa5fff4
jonatan <jonatan.baumgartner@he-arc.ch>
2019-05-09 12:57:43 +0200

    ajout changement de mode dans main

--- dev/Ambi-like_PC/src/ch/hearc/compute/Computation_I.java
+++ dev/Ambi-like_PC/src/ch/hearc/compute/Computation_I.java
@@ -18,21 +18,25 @@ import java.util.logging.Logger;
  * @author Téo
  */
 public abstract class Computation_I implements Runnable {
-    
+
+    public final static String MODE_AMBILIGHT = "ambilight";
+    public final static String MODE_FIXE = "fixe";
+    public final static String MODE_PERSO = "perso";
+
     private boolean running;
 
     @Override
     public abstract void run();
-    
-    public void startComputation(){
+
+    public void startComputation() {
         this.running = true;
     }
-    
-    public void stopComputation(){
+
+    public void stopComputation() {
         this.running = false;
     }
-    
-    public boolean isRunning(){
+
+    public boolean isRunning() {
         return this.running;
     }
 }
