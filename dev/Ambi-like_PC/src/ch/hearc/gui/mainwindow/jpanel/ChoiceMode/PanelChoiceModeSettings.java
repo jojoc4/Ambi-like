151206493dfec7a96b0e18500adbd799da733d9f
Julien Chappuis <julien.chappuis@he-arc.ch>
2019-06-03 12:54:23 +0200

    ajout radiobutton dynamique

--- dev/Ambi-like_PC/src/ch/hearc/gui/mainwindow/jpanel/ChoiceMode/PanelChoiceModeSettings.java
+++ dev/Ambi-like_PC/src/ch/hearc/gui/mainwindow/jpanel/ChoiceMode/PanelChoiceModeSettings.java
@@ -14,6 +14,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.Box;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -29,7 +31,7 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 public class PanelChoiceModeSettings extends JPanel {
 
     private PanelChoiceModeColors panelChoiceModeColors;
-
+    private Box boxVertical;
     private ButtonGroup group;
     private JRadioButton rbAmbilight;
     private JRadioButton rbFixedColor;
@@ -37,6 +39,7 @@ public class PanelChoiceModeSettings extends JPanel {
     private JRadioButton rbCustomMode2;
     private JRadioButton rbCustomMode3;
 
+    private List<JRadioButton> listRadioButton;
     private JButton buttonBrowse;
 
     private File[] listFile;
@@ -55,9 +58,9 @@ public class PanelChoiceModeSettings extends JPanel {
     private void geometry() {
         rbAmbilight = new JRadioButton("Réactif (Ambi-light)");
         rbFixedColor = new JRadioButton("Couleur fixe : ");
-        rbCustomMode1 = new JRadioButton("Mode personnalisé 1");
-        rbCustomMode2 = new JRadioButton("Mode personnalisé 2");
-        rbCustomMode3 = new JRadioButton("Mode personnalisé 3");
+//        rbCustomMode1 = new JRadioButton("Mode personnalisé 1");
+//        rbCustomMode2 = new JRadioButton("Mode personnalisé 2");
+//        rbCustomMode3 = new JRadioButton("Mode personnalisé 3");
 
         panelChoiceModeColors = new PanelChoiceModeColors(this);
 
@@ -66,17 +69,17 @@ public class PanelChoiceModeSettings extends JPanel {
 
         group.add(rbAmbilight);
         group.add(rbFixedColor);
-        group.add(rbCustomMode1);
-        group.add(rbCustomMode2);
-        group.add(rbCustomMode3);
+//        group.add(rbCustomMode1);
+//        group.add(rbCustomMode2);
+//        group.add(rbCustomMode3);
 
-        Box boxVertical = Box.createVerticalBox();
+        boxVertical = Box.createVerticalBox();
         boxVertical.add(rbAmbilight);
         boxVertical.add(rbFixedColor);
         boxVertical.add(panelChoiceModeColors);
-        boxVertical.add(rbCustomMode1);
-        boxVertical.add(rbCustomMode2);
-        boxVertical.add(rbCustomMode3);
+//        boxVertical.add(rbCustomMode1);
+//        boxVertical.add(rbCustomMode2);
+//        boxVertical.add(rbCustomMode3);
         boxVertical.add(buttonBrowse);
 
         setLayout(new BorderLayout());
@@ -110,31 +113,31 @@ public class PanelChoiceModeSettings extends JPanel {
                 }
             }
         });
-        rbCustomMode1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                panelChoiceModeColors.setVisible(false);
-                configFile.setPersoModeFile(listFile[0].getAbsolutePath());
-                Main.setTempMode(Computation_I.MODE_PERSO);
-            }
-        });
-        rbCustomMode2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                panelChoiceModeColors.setVisible(false);
-                configFile.setPersoModeFile(listFile[1].getAbsolutePath());
-                Main.setTempMode(Computation_I.MODE_PERSO);
-
-            }
-        });
-        rbCustomMode3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                panelChoiceModeColors.setVisible(false);
-                configFile.setPersoModeFile(listFile[2].getAbsolutePath());
-                Main.setTempMode(Computation_I.MODE_PERSO);
-            }
-        });
+//        rbCustomMode1.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                panelChoiceModeColors.setVisible(false);
+//                configFile.setPersoModeFile(listFile[0].getAbsolutePath());
+//                Main.setTempMode(Computation_I.MODE_PERSO);
+//            }
+//        });
+//        rbCustomMode2.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                panelChoiceModeColors.setVisible(false);
+//                configFile.setPersoModeFile(listFile[1].getAbsolutePath());
+//                Main.setTempMode(Computation_I.MODE_PERSO);
+//
+//            }
+//        });
+//        rbCustomMode3.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                panelChoiceModeColors.setVisible(false);
+//                configFile.setPersoModeFile(listFile[2].getAbsolutePath());
+//                Main.setTempMode(Computation_I.MODE_PERSO);
+//            }
+//        });
 
         buttonBrowse.addActionListener(new ActionListener() {
             @Override
@@ -157,9 +160,6 @@ public class PanelChoiceModeSettings extends JPanel {
 
     private void appearance() {
         rbAmbilight.setSelected(true);
-        rbCustomMode1.setVisible(false);
-        rbCustomMode2.setVisible(false);
-        rbCustomMode3.setVisible(false);
     }
 
     private void displayModePerso() {
@@ -169,32 +169,50 @@ public class PanelChoiceModeSettings extends JPanel {
                 return name.toLowerCase().endsWith(".amm");
             }
         });
+        listRadioButton = new ArrayList<JRadioButton>();
 
-        if (listFile.length > 0) {
-            if (listFile.length > 0) {
-                rbCustomMode1.setVisible(true);
-                rbCustomMode1.setText((ModePersonnalise.getMode(listFile[0].toString())).getName());
-            }
-            if (listFile.length > 1) {
-                rbCustomMode2.setVisible(true);
-                rbCustomMode2.setText((ModePersonnalise.getMode(listFile[1].toString())).getName());
-            }
-            if (listFile.length > 2) {
-                rbCustomMode3.setVisible(true);
-                rbCustomMode3.setText((ModePersonnalise.getMode(listFile[2].toString())).getName());
-            }
-        } else {
-            rbCustomMode1.setVisible(false);
-            rbCustomMode2.setVisible(false);
-            rbCustomMode3.setVisible(false);
-            rbCustomMode1.setSelected(false);
-            rbCustomMode2.setSelected(false);
-            rbCustomMode3.setSelected(false);
-            rbFixedColor.setSelected(false);
-            rbAmbilight.doClick();
-            
-            
+        for (int i = 0; i < listFile.length; i++) {
+            listRadioButton.add(new JRadioButton(ModePersonnalise.getMode(listFile[i].toString()).getName()));
+            String path = listFile[i].getAbsolutePath();
+            listRadioButton.get(i).setVisible(true);
+            group.add(listRadioButton.get(i));
+
+            boxVertical.add(listRadioButton.get(i));
+            listRadioButton.get(i).addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    panelChoiceModeColors.setVisible(false);
+                    configFile.setPersoModeFile(path);
+                    Main.setTempMode(Computation_I.MODE_PERSO);
+                }
+            });
         }
+        repaint();
 
+//        if (listRadioButton.size() > 0) {
+//            if (listFile.length > 0) {
+//                rbCustomMode1
+//                rbCustomMode1.setText((ModePersonnalise.getMode(listFile[0].toString()).getName()));
+//            }
+//            if (listFile.length > 1) {
+//                rbCustomMode2.setVisible(true);
+//                rbCustomMode2.setText((ModePersonnalise.getMode(listFile[1].toString()).getName()));
+//            }
+//            if (listFile.length > 2) {
+//                rbCustomMode3.setVisible(true);
+//                rbCustomMode3.setText((ModePersonnalise.getMode(listFile[2].toString()).getName()));
+//            }
+//        } else {
+//            rbCustomMode1.setVisible(false);
+//            rbCustomMode2.setVisible(false);
+//            rbCustomMode3.setVisible(false);
+//            rbCustomMode1.setSelected(false);
+//            rbCustomMode2.setSelected(false);
+//            rbCustomMode3.setSelected(false);
+//            rbFixedColor.setSelected(false);
+//            rbAmbilight.doClick();
+//            
+//            
+//        }
     }
 }
