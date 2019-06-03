ef20dffa129a6d78d1046726399a74a6f8ef5023
Téo Schaffner <teo.schaffner@he-arc.ch>
2019-06-03 13:32:11 +0200

    modes personnalisés s'affichent correctement sur la mainwindow

--- dev/Ambi-like_PC/src/ch/hearc/gui/mainwindow/jpanel/ChoiceMode/PanelChoiceModeSettings.java
+++ dev/Ambi-like_PC/src/ch/hearc/gui/mainwindow/jpanel/ChoiceMode/PanelChoiceModeSettings.java
@@ -20,9 +20,9 @@ import javax.swing.Box;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
@@ -35,10 +35,8 @@ public class PanelChoiceModeSettings extends JPanel {
     private ButtonGroup group;
     private JRadioButton rbAmbilight;
     private JRadioButton rbFixedColor;
-    private JRadioButton rbCustomMode1;
-    private JRadioButton rbCustomMode2;
-    private JRadioButton rbCustomMode3;
 
+    private JLabel labelModesPerso;
     private List<JRadioButton> listRadioButton;
     private JButton buttonBrowse;
 
@@ -48,46 +46,42 @@ public class PanelChoiceModeSettings extends JPanel {
     private Config configFile;
 
     public PanelChoiceModeSettings() {
-
         geometry();
         control();
         appearance();
-
     }
 
     private void geometry() {
         rbAmbilight = new JRadioButton("Réactif (Ambi-light)");
         rbFixedColor = new JRadioButton("Couleur fixe : ");
-//        rbCustomMode1 = new JRadioButton("Mode personnalisé 1");
-//        rbCustomMode2 = new JRadioButton("Mode personnalisé 2");
-//        rbCustomMode3 = new JRadioButton("Mode personnalisé 3");
 
         panelChoiceModeColors = new PanelChoiceModeColors(this);
 
         group = new ButtonGroup();
-        buttonBrowse = new JButton("Choisir dossier mode perso");
+        buttonBrowse = new JButton("Choisir emplacement des modes");
+        labelModesPerso = new JLabel("Modes personnalisés :");
+        listRadioButton = new ArrayList<JRadioButton>();
 
         group.add(rbAmbilight);
         group.add(rbFixedColor);
-//        group.add(rbCustomMode1);
-//        group.add(rbCustomMode2);
-//        group.add(rbCustomMode3);
 
         boxVertical = Box.createVerticalBox();
         boxVertical.add(rbAmbilight);
         boxVertical.add(rbFixedColor);
         boxVertical.add(panelChoiceModeColors);
-//        boxVertical.add(rbCustomMode1);
-//        boxVertical.add(rbCustomMode2);
-//        boxVertical.add(rbCustomMode3);
+        boxVertical.add(Box.createVerticalStrut(20));
+        boxVertical.add(labelModesPerso);
+        boxVertical.add(Box.createVerticalStrut(10));
         boxVertical.add(buttonBrowse);
+        boxVertical.add(Box.createVerticalStrut(10));
 
         setLayout(new BorderLayout());
-
         add(boxVertical, BorderLayout.CENTER);
 
         configFile = Config.getConfig();
-
+        
+        folderPath = "./modes";
+        displayModePerso();
     }
 
     private void control() {
@@ -113,31 +107,6 @@ public class PanelChoiceModeSettings extends JPanel {
                 }
             }
         });
-//        rbCustomMode1.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                panelChoiceModeColors.setVisible(false);
-//                configFile.setPersoModeFile(listFile[0].getAbsolutePath());
-//                Main.setTempMode(Computation_I.MODE_PERSO);
-//            }
-//        });
-//        rbCustomMode2.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                panelChoiceModeColors.setVisible(false);
-//                configFile.setPersoModeFile(listFile[1].getAbsolutePath());
-//                Main.setTempMode(Computation_I.MODE_PERSO);
-//
-//            }
-//        });
-//        rbCustomMode3.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                panelChoiceModeColors.setVisible(false);
-//                configFile.setPersoModeFile(listFile[2].getAbsolutePath());
-//                Main.setTempMode(Computation_I.MODE_PERSO);
-//            }
-//        });
 
         buttonBrowse.addActionListener(new ActionListener() {
             @Override
@@ -163,12 +132,17 @@ public class PanelChoiceModeSettings extends JPanel {
     }
 
     private void displayModePerso() {
+        for(JRadioButton jrb : listRadioButton){
+            boxVertical.remove(jrb);
+        }
+        
         File folder = new File(folderPath);
         this.listFile = folder.listFiles(new FilenameFilter() {
             public boolean accept(File dir, String name) {
                 return name.toLowerCase().endsWith(".amm");
             }
         });
+        
         listRadioButton = new ArrayList<JRadioButton>();
 
         for (int i = 0; i < listFile.length; i++) {
@@ -187,32 +161,8 @@ public class PanelChoiceModeSettings extends JPanel {
                 }
             });
         }
+        
+        revalidate();
         repaint();
-
-//        if (listRadioButton.size() > 0) {
-//            if (listFile.length > 0) {
-//                rbCustomMode1
-//                rbCustomMode1.setText((ModePersonnalise.getMode(listFile[0].toString()).getName()));
-//            }
-//            if (listFile.length > 1) {
-//                rbCustomMode2.setVisible(true);
-//                rbCustomMode2.setText((ModePersonnalise.getMode(listFile[1].toString()).getName()));
-//            }
-//            if (listFile.length > 2) {
-//                rbCustomMode3.setVisible(true);
-//                rbCustomMode3.setText((ModePersonnalise.getMode(listFile[2].toString()).getName()));
-//            }
-//        } else {
-//            rbCustomMode1.setVisible(false);
-//            rbCustomMode2.setVisible(false);
-//            rbCustomMode3.setVisible(false);
-//            rbCustomMode1.setSelected(false);
-//            rbCustomMode2.setSelected(false);
-//            rbCustomMode3.setSelected(false);
-//            rbFixedColor.setSelected(false);
-//            rbAmbilight.doClick();
-//            
-//            
-//        }
     }
 }
